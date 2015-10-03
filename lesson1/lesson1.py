b52b8185406b5ab33fe82a6efff7d47528123d4d
KlavierCat <plin@tcd.ie>
2015-10-03 15:47:43 +0100

    lesson 1 example: step 5 - add parameters for length of sides in square function

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -2,19 +2,19 @@
 from turtle import *
 
 # define a function to draw a square with a for loop
-def square():
+def square(length):
   for side in range (4):
-    forward(50)
+    forward(length)
     right(90)
 
 # define a function to handle the moving around part to draw the square
-def moveAndDraw(x, y):
+def moveAndDraw(x, y, length):
   penup()
   goto(x, y)
   pendown()
-  square()
+  square(length)
 
 # call the function to draw the squares several times
-moveAndDraw(0, 0)
-moveAndDraw(90, 90)
-moveAndDraw(-90, -90)
+moveAndDraw(0, 0, 40)
+moveAndDraw(90, 90, 30)
+moveAndDraw(-90, -90, 50)
