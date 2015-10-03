578e70e112c1fcf6dfea66c3b22ef28c4b795bc0
KlavierCat <plin@tcd.ie>
2015-10-03 15:50:52 +0100

    lesson 1 example: step 6 - add more parameters to draw different shapes in different size

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -2,19 +2,19 @@
 from turtle import *
 
 # define a function to draw a square with a for loop
-def square(length):
-  for side in range (4):
+def shape(length, sides):
+  for x in range (sides):
     forward(length)
-    right(90)
+    right(360/sides)
 
 # define a function to handle the moving around part to draw the square
-def moveAndDraw(x, y, length):
+def moveAndDraw(x, y, length, sides):
   penup()
   goto(x, y)
   pendown()
-  square(length)
+  shape(length, sides)
 
 # call the function to draw the squares several times
-moveAndDraw(0, 0, 40)
-moveAndDraw(90, 90, 30)
-moveAndDraw(-90, -90, 50)
+moveAndDraw(0, 0, 40, 4)
+moveAndDraw(90, 90, 30, 3)
+moveAndDraw(-90, -90, 50, 5)
