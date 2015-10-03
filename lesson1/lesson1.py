1785d4622b70b8b801f94caacfde1db747396ff1
KlavierCat <plin@tcd.ie>
2015-10-03 15:19:33 +0100

    lesson 1 example: step 3 - define a function to draw square

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -1,8 +1,23 @@
 # import turtle
 from turtle import *
 
-# to draw a square with a for loop (DRY)
+# define a function to draw a square with a for loop
+def square():
+  for side in range (4):
+    forward(50)
+    right(90)
 
-for side in range (4):
-  forward(50)
-  right(90)
+# call the function to draw a square, or, a few squares
+square()
+
+penup()
+goto(90, 90)
+pendown()
+
+square()
+
+penup()
+goto(-90, -90)
+pendown()
+
+square()
